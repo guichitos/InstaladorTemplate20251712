3b5b8d20f84debf1114c32e335711455848aca51
guichitos <92882295+guichitos@users.noreply.github.com>
2025-12-17 17:16:41 -0400

    Initial commit

---
+++ Script/2. Make MS Office create new files based on default templates.bat
@@ -0,0 +1,72 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBCCbRrM/rxk01btT+fpAWhhGkiDNf2sk=
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBAD3MsOXlxFAuYvfJWZcAmgmyy39cz73T
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBBDLMqsXq2001a9b+fuobiAnX
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBBAHw6dzl3FEGYsn0YLIcm1O1xyU+
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBGzbVpeX2iHYyYdP4aeQNjBCnyjBKy+mepWhHAGJaeQ==
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBGzbWq+DyzXgkd+rpY7Qcmwm+wyIQzKin9w==
+::fBE1pAF6MU+EWHreyHcjLQlHcDShAES0A5EO4f7+r6fHhV8cUOMDWYje1LeHIdwj+ErgYJUu39mJlvdcTDNdbACqYwYxp3pDpVjUeZfckh/tQ0uI5UI/FWBnuzOe3GsSbsB8m88C1y+svEn637Uc0HbrV6UCFHChxakoF88G9AmxVBmIu/NbQ+DrZ/3lDDXJL1UmtUn6xa5H0KcJESh9QxVmuLp+6Dvsf8P2jjkBBDLMqtnqwVcnc9v3YKELxx+20lE=
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFCt3cCuMOU+oD6MZqKW7yPiGpkwhdeU6dozS24i+Mu8X/0bnfDX+2EYK2OMJHglZcxuuYBs1ulIT+DTFFteQugzgSUGG6E4jJzU61yP5gjgvYd9pnswRkyS7vF3znqsE2HTzX7pOEWah7qpuMcoFwVr0SQnGk6VQRrbiZL7oDwqYY1saj2XRmIx5ooAiUTRwQAxlgrpj7xSpEcvqjiUPIDDKxA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF25
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFCt3cCuMOU+oD6MZqKW7yPiGpkwhdeU6dozS24i+Mu8X/0bnfDX+2EYK2OMJHglZcxuuYBs1ulIT+DTFFteQugzgSUGG6E4jJzU61yP5gjgvYd9pnswRkyS7vF3znqsE2HTzX7pOEWah7qpuMcoFwVr0SQnGk6VQRrbiZL7oDwrxGlAmwl3uxadO0IslUi1JHFYavrd86CW4V5e2zHtAPTbW5Or2x1R0SNz9ZaccyRyn1iIexqa+kh0aAXFLHF5VSg+OIA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+rem ------------------------------------------------------------
+rem  OFFICE TEMPLATE INSTALLER - UNINSTALLER WRAPPER
+rem  Purpose:
+rem    Ensures the real uninstaller receives the TRUE launcher
+rem    directory (%~dp0) instead of the temporary EXE extraction.
+rem ------------------------------------------------------------
+
+rem === Determine real launcher directory (where THIS file lives)
+set "LauncherDir=%~dp0"
+rem echo [INFO - make ms] Real launcher directory resolved to: %LauncherDir%
+rem === Location of the internal uninstaller placed by the installer
+set "InternalUninstaller=%APPDATA%\1-2. MainUninstaller.bat"
+
+rem === Validate presence
+if not exist "%InternalUninstaller%" (
+    echo.
+    echo [ERROR] No se encontro el desinstalador interno.
+    echo Ruta esperada:
+    echo     %InternalUninstaller%
+    echo Asegurese de que el instalador inicial se ejecuto correctamente.
+    echo.
+    exit /b 1
+)
+
+rem === Execute internal uninstaller, passing the real launcher dir
+call "%InternalUninstaller%" "%LauncherDir%"
+endlocal
+exit /b 0
+
